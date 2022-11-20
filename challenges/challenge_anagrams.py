a51681a2a6622474bbda253a0862c1e552344cf3
GustavoGracioM <gustavogracio@hotmail.com>
2022-11-20 14:19:32 -0300

    implementa função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,26 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_anagram(first_string: str, second_string: str):
+    fs = ''
+    ss = ''
+    if first_string or second_string:
+        fs = insertion_sort(first_string.lower())
+        ss = insertion_sort(second_string.lower())
+        if fs == ss:
+            return (fs, ss, True)
+    return (fs, ss, False)
+
+
+def insertion_sort(string):
+    list_string = list(string)
+    n = len(list_string)
+
+    for index in range(1, n):
+        key = list_string[index]
+
+        p = index - 1
+        while p >= 0 and list_string[p] > key:
+            list_string[p + 1] = list_string[p]
+            p = p - 1
+
+        list_string[p + 1] = key
+
+    return "".join(list_string)
